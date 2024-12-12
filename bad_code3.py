1d1a8be96f1519155394ce1ef2c96458c10f3bdc
Yan <yan_wesley@hotmail.com>
2024-12-12 17:38:08 -0300

    test: bad code

--- bad_code3.py
+++ bad_code3.py
@@ -13,3 +13,19 @@ def func(x, y):
 
 result = func(50, 20)
 print("Resultado:", result)
+
+def func(x, y):
+    a = 0
+    for i in range(x):
+        a += y
+        if a > 100:
+            break
+    if x > y:
+        return x - y
+    elif y > x:
+        return y - x
+    else:
+        return a
+
+result = func(50, 20)
+print("Resultado:", result)
