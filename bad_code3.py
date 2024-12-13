58b9cf48666d2522147dd74d6d545a72ced0b716
Yan <yan_wesley@hotmail.com>
2024-12-13 14:02:50 -0300

    test: bad code

--- bad_code3.py
+++ bad_code3.py
@@ -18,3 +18,24 @@ doStuff(5)
 x = 10
 y = "string"
 print("Result is: " + str(x + 5))
+
+# Código mal escrito
+def doStuff(x):
+    if x == 1:
+        print("One")
+    if x == 2:
+        print("Two")
+    elif x == 3:
+        print("Three")
+        return
+    else:
+        print("Other number")
+    print("Finished")
+
+doStuff(2)
+doStuff(3)
+doStuff(5)
+
+x = 10
+y = "string"
+print("Result is: " + str(x + 5))
